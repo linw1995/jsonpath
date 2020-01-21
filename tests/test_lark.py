b882c387e366cfb02c3a59a7fa7431ade9d61897
林玮 <linw1995@icloud.com>
2020-01-21 00:40:13 -0600

    Chg:Use lark-parser to replace sly

--- tests/test_lark.py
+++ tests/test_lark.py
@@ -1,20 +1,127 @@
 # Standard Library
 import json
-import reprlib
+import logging
+
+from contextlib import nullcontext as does_not_raise
 
 # Third Party Library
+import _pytest.python_api
 import pytest
 
+from lark import Lark, UnexpectedToken
+
 # First Party Library
-from jsonpath import JSONPathSyntaxError, parse
+from jsonpath.core import JSONPathSyntaxError
+from jsonpath.parser import parse, parser
 
 
-def test_find(expr, data, expect):
-    assert parse(expr).find(data) == expect
+def test_no_conflict(caplog):
+    with caplog.at_level(logging.DEBUG, logger="lark"):
+        Lark.open(
+            "jsonpath/grammar.lark",
+            parser="lalr",
+            debug=True,
+            keep_all_tokens=True,
+        )
+
+    assert len(caplog.records) == 0
 
 
-test_find = pytest.mark.parametrize(
-    "expr,data,expect",
+def ids(x):
+    if isinstance(x, does_not_raise):
+        return "does not raise"
+    elif isinstance(x, _pytest.python_api.RaisesContext):
+        return f"raise {x.expected_exception.__name__}"
+    else:
+        return json.dumps(x)
+
+
+def test_parser_parse(expression, raises_what):
+    with raises_what:
+        tree = parser.parse(expression)
+        logging.debug(f"parser.parse {expression!r} result: {tree.pretty()}")
+
+
+parser_parse_not_raises_exception_testcases = [
+    "a.b",
+    "a.b.c",
+    "a.b.c.d",
+    "a[:3]",
+    "a[:-3]",
+    "a[-1]",
+    "a[0]",
+    "a[*]",
+    "$[@]",
+    "$[$]",
+    "a[b]",
+    "a.*",
+    "a.'b'",
+    "a..[b]",
+    "a..[0]",
+    "a..[:3]",
+    "$.goods[contains(@.category, $.targetCategory)]",
+    "$.goods[@.category]",
+    "$.goods[@.price > 10]",
+    "$.goods[@.price >= 10 and @.price < 20]",
+    "$.goods[@.price >= 10 and @.price < 20 and @.category = $.targetCategory]",
+    "$.goods"
+    "[(@.price >= 10 and @.price < 20) or @.category = $.targetCategory]",
+    "$.goods[@.'price' > 10]",
+    '$."price"',
+    "$.'price'",
+    "$.`price`",
+    "($.*[*])[0]",
+    "($.*)[0]",
+    "$..boo.bar",
+    "$[(type='book' or type='video') and price > 100]",
+    "(a.b).b",
+    "a[(b[0] or b[1]) and (b.c)[0].d]",
+    "a[((b[0]).c or b[1]) and d]",
+    "$[c and (a or b)]",
+    "$[c and ((a or b) or c)]",
+    "((a.b).c).d",
+]
+
+parser_parse_raises_exception_testcases = [
+    "((a.b)).c",
+    "$[((a.b).c)]",
+    "array[]",
+    "$*",
+    "[*]",
+    '"abc"',
+    "'abc'",
+    "`abc`",
+    "$[1 < @]",
+    "$[@ and 1]",
+    "$[(a and b)]",
+    "$[((a and b)) or c]",
+    "$[not((a and b)) or c]",
+]
+parser_parse_testcases = [
+    *(
+        (expression, does_not_raise())
+        for expression in parser_parse_not_raises_exception_testcases
+    ),
+    *(
+        (expression, pytest.raises(UnexpectedToken))
+        for expression in parser_parse_raises_exception_testcases
+    ),
+]
+
+pytest.mark.parametrize(
+    "expression, raises_what", parser_parse_testcases, ids=ids,
+)(test_parser_parse)
+
+
+def test_parse_check_and_extract(expression, data, expect):
+    jp = parse(expression)
+    logging.debug(f"parse {expression!r} result: {jp}")
+    assert str(jp) == expression
+    assert jp.find(data) == expect
+
+
+pytest.mark.parametrize(
+    "expression, data, expect",
     [
         ("boo", {"boo": 1}, [1]),
         ("boo.bar", {"boo": {"bar": 1}}, [1]),
@@ -27,8 +134,9 @@ test_find = pytest.mark.parametrize(
         ("$[0]", [1, 2], [1]),
         ("boo[0]", {"boo": [1, 2]}, [1]),
         ("$[*]", [1, 2], [1, 2]),
-        ("$[0:1]", [1, 2], [1]),
-        ("$[0:]", [1, 2], [1, 2]),
+        ("$[:1]", [1, 2], [1]),
+        ("$[1:2]", [1, 2], [2]),
+        ("$[:]", [1, 2], [1, 2]),
         ("$[:-1]", [1, 2, 3], [1, 2]),
         ("$[::2]", [1, 2, 3], [1, 3]),
         ("$.*[0]", {"boo": [1, 2, 3], "bar": [2, 3, 4]}, [1, 2]),
@@ -54,6 +162,7 @@ test_find = pytest.mark.parametrize(
             [{"boo": [1, 2, 3, 4, 5]}, 1, 3],
         ),
         ("@", "abc", ["abc"]),
+        ("$[@ < 10]", [0, 10, 12, 1, 3], [0, 1, 3]),
         (
             "$[@.price > 100]",
             [{"price": 100}, {"price": 200}],
@@ -101,7 +210,7 @@ test_find = pytest.mark.parametrize(
             [{"result": {"result": "result"}}, {"result": "result"}],
         ),
         (
-            "$..[price>100]",
+            "$..[price > 100]",
             {
                 "price": 200,
                 "charpter": [{"price": 100}, {"price": 200}, {"price": 300}],
@@ -119,63 +228,48 @@ test_find = pytest.mark.parametrize(
                 {"price": 300},
             ],
         ),
-        ("$[price>100.5]", [{"price": 100}, {"price": 200}], [{"price": 200}],),
-        ("$[on=null]", [{"on": None}, {"on": False}], [{"on": None}],),
-        ("$[on=true]", [{"on": True}, {"on": False}], [{"on": True}],),
-        ("$[on=false]", [{"on": True}, {"on": False}], [{"on": False}],),
         (
-            "$.systems[on=$.on]",
+            "$[price > 100.5]",
+            [{"price": 100}, {"price": 200}],
+            [{"price": 200}],
+        ),
+        ("$[on = null]", [{"on": None}, {"on": False}], [{"on": None}],),
+        ("$[on = true]", [{"on": True}, {"on": False}], [{"on": True}],),
+        ("$[on = false]", [{"on": True}, {"on": False}], [{"on": False}],),
+        (
+            "$.systems[on = $.on]",
             {"systems": [{"on": True}, {"on": False}], "on": False},
             [{"on": False}],
         ),
         (
-            "$.systems[on=$.notexists]",
+            "$.systems[on = $.notexists]",
             {"systems": [{"on": True}, {"on": False}], "on": False},
             [],
         ),
         (
-            "$[name='john']",
+            '$[name = "john"]',
             [{"name": "jack"}, {"name": "john"}],
             [{"name": "john"}],
         ),
         (
-            '$[name="john"]',
+            '$[name = "john"]',
             [{"name": "jack"}, {"name": "john"}],
             [{"name": "john"}],
         ),
+        ("$[*].name", [{"name": "jack"}, {"name": "john"}], ["jack", "john"],),
         (
-            """$[name='"john"']""",
-            [{"name": "jack"}, {"name": '"john"'}],
-            [{"name": '"john"'}],
-        ),
-        (
-            """$[name="'john'"]""",
-            [{"name": "jack"}, {"name": "'john'"}],
-            [{"name": "'john'"}],
-        ),
-        (
-            """$[name=`john'`]""",
-            [{"name": "jack"}, {"name": "john'"}],
-            [{"name": "john'"}],
-        ),
-        (
-            """$[*].'name'""",
-            [{"name": "jack"}, {"name": "john"}],
-            ["jack", "john"],
-        ),
-        (
-            "$[key()='bookA']",
+            '$[key() = "bookA"]',
             {"bookA": {"price": 100}, "bookB": {}},
             [{"price": 100}],
         ),
-        ("$[key()=0]", [{"price": 100}, {"price": 200}], [{"price": 100}],),
+        ("$[key() = 0]", [{"price": 100}, {"price": 200}], [{"price": 100}],),
         (
-            "$[contains(key(),'book')]",
+            '$[contains(key(), "book")]',
             {"bookA": {"price": 100}, "bookB": {"price": 200}, "pictureA": {}},
             [{"price": 100}, {"price": 200}],
         ),
         (
-            "$[contains(@.category,'book')]",
+            '$[contains(@.category, "book")]',
             [
                 {"price": 100, "category": "Comic book"},
                 {"price": 200, "category": "magazine"},
@@ -196,12 +290,12 @@ test_find = pytest.mark.parametrize(
             [{"price": 100, "category": "Comic book"}],
         ),
         (
-            "$[contains(@.category,$.targetCategory)]",
+            "$[contains(@.category, $.targetCategory)]",
             [{"price": 100, "category": "Comic book"}],
             [],
         ),
         (
-            "$[type='book' and price > 100]",
+            '$[type = "book" and price > 100]',
             [
                 {"type": "book", "price": 100},
                 {"type": "book", "price": 200},
@@ -210,7 +304,7 @@ test_find = pytest.mark.parametrize(
             [{"type": "book", "price": 200}],
         ),
         (
-            "$[(type='book' or type='video') and price > 100]",
+            '$[(type = "book" or type = "video") and price > 100]',
             [
                 {"type": "book", "price": 100},
                 {"type": "book", "price": 200},
@@ -219,7 +313,7 @@ test_find = pytest.mark.parametrize(
             [{"type": "book", "price": 200}, {"type": "video", "price": 200}],
         ),
         (
-            "$[type='book' or type='video']",
+            '$[type = "book" or type = "video"]',
             [
                 {"type": "book", "price": 100},
                 {"type": "book", "price": 200},
@@ -233,7 +327,7 @@ test_find = pytest.mark.parametrize(
             ],
         ),
         (
-            "$[type='book' or type='video' or type='audio']",
+            '$[type = "book" or type = "video" or type = "audio"]',
             [
                 {"type": "book", "price": 200},
                 {"type": "video", "price": 200},
@@ -263,7 +357,7 @@ test_find = pytest.mark.parametrize(
             [{"is": 1}, {"is": True}, {"is": "str"}, {"is": 1.1}],
         ),
         (
-            "$[contains(@, 'is')]",
+            '$[contains(@, "is")]',
             [
                 {"is": 1},
                 {"is": 0},
@@ -315,7 +409,7 @@ test_find = pytest.mark.parametrize(
             ],
         ),
         (
-            "$[not(contains(@, 'is'))]",
+            '$[not(contains(@, "is"))]',
             [
                 {"is": 1},
                 {"is": 0},
@@ -332,7 +426,7 @@ test_find = pytest.mark.parametrize(
             [{}],
         ),
         (
-            "$[not(type='book' or type='video')]",
+            '$[not(type = "book" or type = "video")]',
             [
                 {"type": "book", "price": 100},
                 {"type": "video", "price": 200},
@@ -341,11 +435,48 @@ test_find = pytest.mark.parametrize(
             [{"type": "audio", "price": 100}],
         ),
     ],
-    ids=lambda x: reprlib.repr(json.dumps(x)),
-)(test_find)
+    ids=ids,
+)(test_parse_check_and_extract)
 
 
-@pytest.mark.parametrize("expr", ["array[]", "$*", "[*]"])
-def test_syntax_error(expr):
+def test_parse_and_extract(expression, data, expect):
+    jp = parse(expression)
+    logging.debug(f"parse {expression!r} result: {jp}")
+    assert jp.find(data) == expect
+
+
+pytest.mark.parametrize(
+    "expression, data, expect",
+    [
+        ('$."*"', {"*": ""}, [""]),
+        ("$.'*'", {"*": ""}, [""]),
+        ("$.`*`", {"*": ""}, [""]),
+        (
+            """$[name='"john"']""",
+            [{"name": "jack"}, {"name": '"john"'}],
+            [{"name": '"john"'}],
+        ),
+        (
+            """$[name="'john'"]""",
+            [{"name": "jack"}, {"name": "'john'"}],
+            [{"name": "'john'"}],
+        ),
+        (
+            """$[name=`john'`]""",
+            [{"name": "jack"}, {"name": "john'"}],
+            [{"name": "john'"}],
+        ),
+        (
+            """$[*].'name'""",
+            [{"name": "jack"}, {"name": "john"}],
+            ["jack", "john"],
+        ),
+    ],
+    ids=ids,
+)(test_parse_and_extract)
+
+
+@pytest.mark.parametrize("expression", parser_parse_raises_exception_testcases)
+def test_syntax_error(expression):
     with pytest.raises(JSONPathSyntaxError):
-        parse(expr)
+        parse(expression)
