48ad21ca013b435fe5133965e05a5b89600f21aa
林玮 <linw1995@icloud.com>
2020-02-24 22:29:00 +0800

    Fix,Dev:shell function not inherits envs of parent process

--- scripts/watch_build_and_serve_html_docs.py
+++ scripts/watch_build_and_serve_html_docs.py
@@ -1,8 +1,35 @@
 """
 Watch related files, build and serve Sphinx documentation automatically.
 """
+# Standard Library
+import os
+import shlex
+import subprocess
+
 # Third Party Library
-from livereload import Server, shell
+from livereload import Server
+
+
+def shell(cmd, output=None, cwd=None, env=None, shell=False):
+    if output is None:
+        output = subprocess.DEVNULL
+
+    if not isinstance(cmd, (list, tuple)) and not shell:
+        cmd = shlex.split(cmd)
+
+    def run():
+        p = subprocess.Popen(
+            cmd,
+            stdin=subprocess.DEVNULL,
+            stdout=output,
+            stderr=output,
+            cwd=cwd,
+            shell=shell,
+            env=env or os.environ.copy(),
+        )
+        p.wait()
+
+    return run
 
 
 def main():
