c953055134f516e7e2e4eb9313bf4879d5128b9c
林玮 <linw1995@icloud.com>
2020-04-24 01:06:18 +0800

    Fix:Lint error

--- jsonpath/lark.py
+++ jsonpath/lark.py
@@ -1,7 +1,25 @@
+# Standard Library
+from typing import Any, Dict
+
+
 try:
-    from lark import *
-    from lark.exceptions import *
+    from lark import Transformer, v_args, Lark
+    from lark.exceptions import UnexpectedToken, VisitError
+
+    Lark_StandAlone = Lark
+    DATA: Dict[str, Any] = {}
 except ImportError:
     import importlib.resources
 
     exec(importlib.resources.read_text(__name__.split(".")[0], "lark"))
+
+
+__all__ = (
+    "Transformer",
+    "v_args",
+    "Lark",
+    "Lark_StandAlone",
+    "DATA",
+    "UnexpectedToken",
+    "VisitError",
+)
